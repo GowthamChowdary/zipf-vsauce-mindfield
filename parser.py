8531a326ae63c587ba9975eba9238b3b6c13089d
Gowtham Chowdary <42214663+GowthamChowdary@users.noreply.github.com>
2020-08-12 00:27:46 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+## wanted to do it with reddit comments first, but the comments were too messy and took way too long to clean them, and testing it on vsauce made alot of sense so I did it with vsauce
+## this basically opens the comments file from pushapi and counts and writes the comments to a text file
+
 import json
 import io
 comments = []
